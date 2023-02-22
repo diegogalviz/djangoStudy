b3d62b16627c814eb213bc3303c74da8bf25068d
diegodev <dagalviz83@misena.edu.co>
2023-02-21 20:51:24 -0500

    gestion de pediodos V03

--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Cliente(models.Model):
@@ -8,10 +9,15 @@ class Cliente(models.Model):
     email = models.EmailField()
     tel = models.CharField(max_length=10)
 
+
 class Articulo(models.Model):
     nombre = models.CharField(max_length=50)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
+
+    def __str__(self):
+        return f'el nombre del articulo es: {self.nombre}, la seccion del articulo es:{self.seccion}, el precio del articulo es{self.precio}'
+
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
