3432e07a321222378eaa044a1b47698951151f82
diegogalviz <arqdiegogalvizv@gmail.com>
2023-02-24 16:46:55 -0500

    tienda v07

--- tiendaOnline/gestionPedidos/admin.py
+++ tiendaOnline/gestionPedidos/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-
+from gestionPedidos.models import Cliente, Articulo, Pedidos
 # Register your models here.
-#
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'email' ,'tel')
+
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'seccion','precio')
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Articulo,ArticuloAdmin)
+admin.site.register(Pedidos)
--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -5,13 +5,16 @@ from django.db import models
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    direccion = models.CharField(max_length=50, verbose_name='La dirección')
+    email = models.EmailField(blank=True, null=True)
     tel = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Articulo(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50 )
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
 
@@ -23,3 +26,6 @@ class Pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
     entrgado = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.numero} - {self.fecha}'
