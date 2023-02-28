5a85fbba6ffccecdcd4433318ade076f4ce1ae02
diegogalviz <arqdiegogalvizv@gmail.com>
2023-02-27 19:04:56 -0500

    tienda v09

--- tiendaOnline/gestionPedidos/views.py
+++ tiendaOnline/gestionPedidos/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulo
 
 
 # Create your views here.
 
 def busqueda_articulo(request):
     return render(request, 'busqueda_articulo.html')
+
+
 def buscar(request):
-    mensaje = f'Articulo buscado: {request.GET["Articulo"]} '
+    if request.GET['Articulo']:
+        # mensaje = f'Articulo buscado: {request.GET["Articulo"]} '
+        producto = request.GET['Articulo']
+        articulos = Articulo.objects.filter(nombre__icontains=producto)
+
+        ctx = {'articulos': articulos, 'query': producto}
+
+        return render(request, 'resultados_busqueda.html', ctx)
+
+    else:
+        mensaje = ' no has introducido nada'
+
     return HttpResponse(mensaje)
