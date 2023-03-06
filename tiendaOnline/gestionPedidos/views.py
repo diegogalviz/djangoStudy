747ad1d8869c52a029e8d782f2d3dc3e5e2a5d44
diegogalviz <arqdiegogalvizv@gmail.com>
2023-03-05 20:40:40 -0500

    tienda v11

--- tiendaOnline/gestionPedidos/views.py
+++ tiendaOnline/gestionPedidos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionPedidos.models import Articulo
+from django.conf import settings
+from django.core.mail import send_mail
+from gestionPedidos.forms import FormularioContacto
 
 
 # Create your views here.
@@ -30,8 +33,27 @@ def buscar(request):
 
     return HttpResponse(mensaje)
 
-def contacto(request):
 
+def contacto(request):
     if request.method == 'POST':
-        return HttpResponse('gracias por enviar su mensaje')
-    return render(request, 'contacto.html')
+
+        miFormulario = FormularioContacto(request.POST)
+
+        if miFormulario.is_valid():
+            infForm = miFormulario.cleaned_data
+            send_mail(infForm['asunto'], infForm['mensaje'], infForm.get('email', ' '), ['arqdiegogalvizv@gmail.com'])
+            return HttpResponse('gracias por enviar su mensaje')
+    else:
+        miFormulario = FormularioContacto()
+
+    ctx = {'form': miFormulario}
+    return render(request, 'formulario_contacto.html', ctx)
+
+    # if request.method == 'POST':
+    #     subject = request.POST['asunto']
+    #     message = request.POST['mensaje'] + " " + request.POST['email']
+    #     email_from = settings.EMAIL_HOST_USER
+    #     recipient_list=['arqdiegogalvizv@gmail.com']
+    #     send_mail(subject, message, email_from, recipient_list, fail_silently=False)
+    #     return HttpResponse('gracias por enviar su mensaje')
+    # return render(request, 'contacto.html')
